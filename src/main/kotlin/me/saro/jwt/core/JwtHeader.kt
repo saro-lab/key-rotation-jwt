64736f9fa02f583d6254bbdd1b95bc057b434ddd
PARK Yong Seo <j@saro.me>
2022-05-08 22:18:13 +0900

    Jwt Header

---
+++ src/main/kotlin/me/saro/jwt/core/JwtHeader.kt
@@ -0,0 +1,31 @@
+package me.saro.jwt.core
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+class JwtHeader private constructor(
+    private val header: Map<String, Any>
+) {
+    companion object {
+        private val OBJECT_MAPPER = ObjectMapper()
+
+        @JvmStatic
+        fun create(algorithm: String): JwtHeader =
+            JwtHeader(mapOf("typ" to "JWT", "alg" to algorithm))
+
+        @JvmStatic
+        fun create(algorithm: String, kid: Any): JwtHeader =
+            JwtHeader(mapOf("typ" to "JWT", "alg" to algorithm, "kid" to kid))
+    }
+
+    val kid: String? get() = get("kid") as String?
+
+    val type: String? get() = get("typ") as String?
+
+    val algorithm: String? get() = get("alg") as String?
+
+    fun get(key: String): Any? = header[key]
+
+    fun toMap(): MutableMap<String, Any> = header.toMutableMap()
+
+    override fun toString(): String = OBJECT_MAPPER.writeValueAsString(header)
+}
