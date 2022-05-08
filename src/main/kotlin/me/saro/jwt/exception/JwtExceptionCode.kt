58b6a19ba820a58c79af777ed4241aae46406cf4
PARK Yong Seo <j@saro.me>
2022-05-08 23:53:31 +0900

    toJsonString

--- src/main/kotlin/me/saro/jwt/exception/JwtExceptionCode.kt
+++ src/main/kotlin/me/saro/jwt/exception/JwtExceptionCode.kt
@@ -1,5 +1,13 @@
 package me.saro.jwt.exception
 
 enum class JwtExceptionCode {
-    PARSE_ERROR
+    // PARSER
+    PARSE_ERROR,
+
+    // HEADER
+    NOT_DEFINED_HEADER_ALGORITHM,
+    NOT_EQUALS_HEADER_ALGORITHM,
+
+    // CLAIMS
+    EXPIRED_DATE
 }
